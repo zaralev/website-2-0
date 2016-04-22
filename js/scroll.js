19e5d0ab291cb5f44d45181e7eead82652caaac8
ZARA <ZLEVENTHAL@GMAIL.COM>
2016-04-22 18:11:13 -0400

    added jquery mouseover animations

--- js/scroll.js
+++ js/scroll.js
@@ -10,6 +10,49 @@ $(document).ready(function() {
         prev = scrollTop;
     });
 
+    $(".block:first-child").mouseover(function(){
+      $(this).css("background-size","40em");
+    });
+    $(".block:first-child").mouseout(function(){
+      $(this).css("background-size","20em");
+    });
+
+    $(".block:nth-child(2)").mouseover(function(){
+      $(this).css("background-size","50em");
+    });
+    $(".block:nth-child(2)").mouseout(function(){
+      $(this).css("background-size","30em");
+    });
+
+    $(".block:nth-child(3)").mouseover(function(){
+      $(this).css("background-size","20em");
+    });
+    $(".block:nth-child(3)").mouseout(function(){
+      $(this).css("background-size","13em");
+    });
+
+    $(".block:nth-child(4)").mouseover(function(){
+      $(this).css("background-size","40em");
+    });
+    $(".block:nth-child(4)").mouseout(function(){
+      $(this).css("background-size","25em");
+    });
+
+    $(".block:nth-child(5)").mouseover(function(){
+      $(this).css("background-size","30em");
+    });
+    $(".block:nth-child(5)").mouseout(function(){
+      $(this).css("background-size","18em");
+    });
+
+    $(".block:nth-child(6)").mouseover(function(){
+      $(this).css("background-size","50em");
+    });
+    $(".block:nth-child(6)").mouseout(function(){
+      $(this).css("background-size","30em");
+    });
+
+
 
     // checkSize();
     //   $(window).resize(checkSize);
