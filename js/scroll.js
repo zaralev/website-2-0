a9bf59ad9e517d5abc0dbd0998edc66e752d3e19
ZARA <ZLEVENTHAL@GMAIL.COM>
2016-04-21 23:04:24 -0400

    couldn't fix nav issue

--- js/scroll.js
+++ js/scroll.js
@@ -9,4 +9,25 @@ $(document).ready(function() {
         nav.toggleClass('hidden', scrollTop > prev);
         prev = scrollTop;
     });
+
+
+    // checkSize();
+    //   $(window).resize(checkSize);
+    //
+    //   function checkSize() {
+    //       if ($(".media").css("color") == "red") {
+    //           console.log("function went through! nav should work!");
+    //           $window.on('scroll', function() {
+    //               var scrollTop = $window.scrollTop();
+    //               nav.toggleClass('hidden', scrollTop > prev);
+    //               prev = scrollTop;
+    //           });
+    //
+    //       } else {
+    //         var color = $(".media").css('color');
+    //         console.log(color);
+    //           console.log("nav shouldn't hide");
+    //       }
+    //   }
+
 });
