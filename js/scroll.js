2cf671ee4f67b29014ead0993f2d6499da9921ba
ZARA <ZLEVENTHAL@GMAIL.COM>
2016-04-22 18:38:20 -0400

    added cute closet modal and swf

--- js/scroll.js
+++ js/scroll.js
@@ -52,25 +52,4 @@ $(document).ready(function() {
       $(this).css("background-size","30em");
     });
 
-
-
-    // checkSize();
-    //   $(window).resize(checkSize);
-    //
-    //   function checkSize() {
-    //       if ($(".media").css("color") == "red") {
-    //           console.log("function went through! nav should work!");
-    //           $window.on('scroll', function() {
-    //               var scrollTop = $window.scrollTop();
-    //               nav.toggleClass('hidden', scrollTop > prev);
-    //               prev = scrollTop;
-    //           });
-    //
-    //       } else {
-    //         var color = $(".media").css('color');
-    //         console.log(color);
-    //           console.log("nav shouldn't hide");
-    //       }
-    //   }
-
 });
